ef7f0f800eecca31a164608e4ca45f68eff84587
Rahul Jaryal <rahuljaryal@Rahuls-MacBook-Pro.local>
2022-11-21 19:22:02 -0600

    Added class instances and demonstrations of class features

--- A3.java
+++ A3.java
@@ -7,10 +7,12 @@ import java.io.*;
  *  Name:           Rahul
  *  Class:          CS30S
  * 
- *  Assignment:     Ax Qy
+ *  Assignment:     A3 Q1
  * 
- *  Description:    Place a short description of your program here
- * 
+ *  Description:    Program demonstrates the use of OOP.
+ *                  The Employee class has setters and getter
+ *                  methods to set and get different attributes/fields.
+ *                  It also has a full-arg and no-arg constructor.
  *************************************************************/
 
 public class A3 {
@@ -20,8 +22,8 @@ public class A3 {
     
     // ***** variables *****
 
-        int assignmentNumber = 0;       // Assignement number
-        int questionNumber = 0;         // Question number
+        int assignmentNumber = 3;       // Assignement number
+        int questionNumber = 1;         // Question number
     
         // String banner = "";             // output banner
         // String prompt = "";             // prompt for user input
@@ -34,6 +36,8 @@ public class A3 {
         
         // a new line character that works on every computer system
         String nl = System.lineSeparator();
+
+        Employee[] employees = new Employee[5];     // List to hold 5 employees
     
     // ***** objects *****
     
@@ -55,7 +59,81 @@ public class A3 {
         // echo input back to console window
     
     // ***** Main Processing *****
-    
+
+        employees[0] = new Employee();                          // Employee 1, with no arg
+        employees[1] = new Employee(11.95, 20);     // Employee 2
+        employees[2] = new Employee(20, 50);        // Employee 3 with OT hours
+        employees[3] = new Employee(15.55, 60);     // Employee 4 with OT hours
+        employees[4] = new Employee(11.95, 41);     // Employee 5 with OT hours
+
+        // Print table
+        System.out.format("%-6s %-5s %-9s %-9s %-9s %-9s", "ID", "HOURS", "WAGE", "R. Pay", "O.T. Pay", "G. Pay");
+        System.out.println();   // Blank line for spacing
+        for(Employee e : employees) {
+            // Use custom toString method to get all fields and information of each class instance
+            System.out.println(e.toString());
+        }
+
+        System.out.println();   // Blank line for spacing
+        System.out.println();   // Blank line for spacing
+        
+
+        // Demonstrates wage getter and setter
+        System.out.println("Employee 1 Wage = " + employees[0].getWage());
+        System.out.println("  SET NEW WAGE");
+        employees[0].setWage(21);   // Set wage to $21.00
+        System.out.println("Employee 1 Wage = " + employees[0].getWage());
+        
+        System.out.println();   // Blank line for spacing
+        
+        
+        // Demonstrates ID and regular pay getter
+        System.out.println("Employee 2 ID = " + employees[1].getId());
+        System.out.println("Employee 2 Regular Pay = " + employees[1].getRegularPay());
+        
+        System.out.println();   // Blank line for spacing
+
+
+        // Demonstrates hours, OT hours, OT pay getters and hours setter
+        System.out.println("Employee 3 Hours = " + employees[2].getHours());
+        System.out.println("Employee 3 O.T. Hours = " + employees[2].getOvertimeHours());
+        System.out.println("Employee 3 O.T Pay = " + employees[2].getOvertimePay());
+        System.out.println("  SET NEW HOURS");
+        employees[2].setHours(80);  // Set hours to 80
+        System.out.println("Employee 3 Hours = " + employees[2].getHours());
+        System.out.println("Employee 3 O.T. Hours = " + employees[2].getOvertimeHours());
+        System.out.println("Employee 3 O.T Pay = " + employees[2].getOvertimePay());
+        
+        System.out.println();   // Blank line for spacing
+        
+
+        // Demonstrates ID, hours, wage, gross pay getters
+        System.out.format("Employee 4 Id = %d, Hours = %d, Wage = $%.2f, Gross Pay = $%.2f" + nl,
+                            employees[3].getId(),
+                            employees[3].getHours(),
+                            employees[3].getWage(),
+                            employees[3].getGrossPay());
+        System.out.println("  CHANGE HOURS AND WAGE");
+        employees[3].setHours(1);   // Set hours to 1
+        employees[3].setWage(1);    // Set wage to 1
+        System.out.format("Employee 4 Id = %d, Hours = %d, Wage = $%.2f, Gross Pay = $%.2f" + nl,
+                            employees[3].getId(),
+                            employees[3].getHours(),
+                            employees[3].getWage(),
+                            employees[3].getGrossPay());
+                            
+        System.out.println();   // Blank line for spacing
+        System.out.println();   // Blank line for spacing
+
+        // Reprint table with new changes and pays calculated
+        System.out.println("  REPRINT TABLE");
+        System.out.format("%-6s %-5s %-9s %-9s %-9s %-9s", "ID", "HOURS", "WAGE", "R. Pay", "O.T. Pay", "G. Pay");
+        System.out.println();   // Blank line for spacing
+        for(Employee e : employees) {
+            // Use custom toString method to get all fields and information of each class instance
+            System.out.println(e.toString());
+        }
+
     // ***** Print Formatted Output *****
     
     // ***** Closing Message *****
